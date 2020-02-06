598f09f37d27c290b0c9d600db2cf36cc1e1de59
dasblitz <arjen@dasblitz.nl>
2020-02-06 22:02:56 +0100

    add some scss styling

---
+++ src/client/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="$emit('click')">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    label: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+button {
+  font-size: 20rem;
+  font-weight: 400;
+  border: 3px solid $COLOR__green;
+  background-color: $COLOR__green;
+  color: white;
+  border-radius: 2px;
+  padding: 8rem 12rem;
+  transition: all 0.25s ease-in-out;
+}
+
+button:hover {
+  background-color: $COLOR__white;
+  color: $COLOR__green;
+}
+</style>
